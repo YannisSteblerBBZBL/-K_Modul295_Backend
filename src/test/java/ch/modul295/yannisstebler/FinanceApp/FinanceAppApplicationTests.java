c87eecef385796d3b9683bde6a9a05755f7601df
Yannis Stebler <yannis.stebler@bbzbl-it.ch>
2025-04-10 10:39:20 +0200

    initialized Spring-Boot project

---
+++ src/test/java/ch/modul295/yannisstebler/FinanceApp/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.modul295.yannisstebler.FinanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
