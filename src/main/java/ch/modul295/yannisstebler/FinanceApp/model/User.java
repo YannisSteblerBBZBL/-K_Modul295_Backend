ce7e60a94afbef5ecc94fdf972ff5477a9897655
Yannis Stebler <yannis.stebler@bbzbl-it.ch>
2025-04-11 09:46:21 +0200

    changed Model Validation

--- src/main/java/ch/modul295/yannisstebler/FinanceApp/model/User.java
+++ src/main/java/ch/modul295/yannisstebler/FinanceApp/model/User.java
@@ -5,14 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.ws.rs.DefaultValue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -23,18 +27,30 @@ public class User {
     @Column(nullable = false)
     String lastName;
 
+    @NotBlank(message = "Username cannot be empty")
     @Column(nullable = false, unique = true)
     String username;
 
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     @Column(nullable = false)
     String password;
 
+    @NotBlank(message = "Keycloak ID cannot be empty")
     @Column(nullable = false)
     String keycloakID;
 
-    @Column(nullable = false, unique = true)
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
     String email;
 
-    @DefaultValue("true")
+    @Column(nullable = false)
     Boolean active;
+
+    @PrePersist
+    public void setDefaults() {
+        if (this.active == null) {
+            this.active = true;
+        }
+    }
 }
